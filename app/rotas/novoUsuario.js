4890a98a3c6f3c292b85cf1ea49ded8a196beff8
lnsantos <39661894+lnsantos@users.noreply.github.com>
2019-05-20 15:06:48 -0300

    Remoção de arquivo + nova tela
    * Removendo arquivo duplicado.
    ** Adicionando o esqueleto para fazer um formulário para cadastrar usuários.

--- app/rotas/novoUsuario.js
+++
@@ -1,7 +0,0 @@
-module.exports =(app)=>{
-    
-    app.get('/novo_usuario',(req, res)=>{
-        var conexao = app.config.database();
-        res.send("<h1>cadastrar novo usuario</h1>");
-    });
-}
---
+++ app/rotas/usuario/novo_usuario.js
@@ -0,0 +1,9 @@
+module.exports =(app)=>{
+    app.get('/novo_usuario', (req, res)=>{
+        res.render("./administrador/usuario/novo_usuario");
+    });
+    app.post('/novo_usuario/salvar',(req, res)=>{
+        var data = req.body;
+        res.send(data);
+    });
+}
